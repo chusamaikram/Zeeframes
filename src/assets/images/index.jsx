45c7c0443e17983d600d4fda5a39a567e9ffc225
chusamaikram <usamachaudhary0345@gmail.com>
2026-01-21 19:00:43 +0500

    updated home insights cards from static to dynamic

--- src/assets/images/index.jsx
+++ src/assets/images/index.jsx
@@ -18,10 +18,6 @@ import effortless from "../images/effortless-shopping.png"
 import ConnectBryond from "../images/connect-beyond.png"
 import style from "../images/style.png"
 import SmartShipping from "../images/smart-shipping.png"
-import insight1 from "../images/insight1.png"
-import insight2 from "../images/insight2.png"
-import insight3 from "../images/insight3.png"
-
 
 
 
@@ -46,8 +42,4 @@ export {
         ConnectBryond,
         style,
         SmartShipping,
-        insight1,
-        insight2,
-        insight3,
-              
 }
--- src/pages/home/insights.jsx
+++ src/pages/home/insights.jsx
@@ -1,39 +1,22 @@
 import { Link } from "react-router-dom";
-import { ArrowIcon } from "../../assets/svg";
-import { insight1, insight2, insight3 } from "../../assets/images";
 import Button from "../../components/common/button";
+import { useEffect, useState } from "react";
 
 export default function InsightsSection() {
-    const InsightCards = [
-        {
-            id: 1,
-            img: insight1,
-            title: "20+ GenAI UX patterns, examples and implementation tactics",
-            desc: "A shared language for product teams to build usable, intelligent and safe GenAI experiences...",
-            timerequire: "7 min read",
-            date: "Jan 23, 2025",
-            path: "/insights/insight-details",
-        },
-        {
-            id: 2,
-            img: insight2,
-            title: "Skipping Figma, human after all, Figma grid, accessibility as an afterthought",
-            desc: "Weekly curated resources for designers — thinkers and makers.",
-            timerequire: "7 min read",
-            date: "Jan 23, 2025",
-            path: "/insights/insight-details",
-        },
-        {
-            id: 3,
-            img: insight3,
-            title: "Is your creative character being sacrificed to Algorithm, Inc.?",
-            desc: "AI is changing how we work. Here’s why and how we must preserve the human spirit that drives...",
-            timerequire: "7 min read",
-            date: "Jan 23, 2025",
-            path: "/insights/insight-details",
-        },
-    ]
+    const [homeinsights, setHomeInsights] = useState([])
+
+    useEffect(() => {
+        if (homeinsights.length === 0) {
+            fetch(`/data/insights.json`)
+                .then(res => res.json())
+                .then(data => {
+                    setHomeInsights(data.data.slice(2, 5))
+                })
+        }
+    }, [])
+
     return (
+
         <>
             <section className="above-glow py-8 sm:py-[80px]">
                 <div className="container">
@@ -43,7 +26,7 @@ export default function InsightsSection() {
                             <h2 className="text-[28px] sm:text-[40px] text-center sm:text-start font-bold leading-[40px] sm:leading-[44px] font-[Geologica]">Insights That Inspire Vision Forward</h2>
                         </div>
                         <div>
-                           
+
                             <Button
                                 defaulttext="Explore More"
                                 hovertext="See more "
@@ -54,8 +37,8 @@ export default function InsightsSection() {
                         </div>
                     </div>
                     <div className="pt-12 grid grid-cols-1 sm:grid-cols-3 gap-8 items-start">
-                        {InsightCards.map(item => (
-                            <Link to={item.path} key={item.id} className="border-2 border-transparent hover:border-[#F3FE00] transition-colors duration-300 rounded-[12px]">
+                        {homeinsights.map(item => (
+                            <Link to={`/insights/${item.slug}`} key={item.slug} className="border-2 border-transparent hover:border-[#F3FE00] transition-colors duration-300 rounded-[12px]">
                                 <div
                                     className="
                                             bg-[#121212] group
@@ -63,11 +46,11 @@ export default function InsightsSection() {
                                             overflow-hidden flex flex-col items-center justify-center
                                             "
                                     aria-label="insight link"  >
-                                    <div className="h-[227px] overflow-hidden">
+                                    <div className="h-[227px] overflow-hidden w-full">
                                         <img
-                                            className="h-full w-full object-cover"
-                                            src={item.img}
-                                            alt={item.title}
+                                            className="h-full w-full object-cover group-hover:scale-105 transition-all duration-400 "
+                                            src={item.thumbnail}
+                                            alt={item.slug}
                                             width={392}
                                             height={227}
                                             loading="lazy"
@@ -76,16 +59,16 @@ export default function InsightsSection() {
 
                                     <div className="h-full p-6 flex flex-col items-start">
                                         <div className="flex flex-col items-start gap-2">
-                                            <h3 className="text-[20px] sm:text-[24px] font-[Geologica] line-clamp-3 font-medium text-white group-hover:text-[#F3FE00]">
+                                            <h3 className="text-[20px] sm:text-[24px] font-[Geologica] leading-[normal] line-clamp-3 font-medium text-white group-hover:text-[#F3FE00]">
                                                 {item.title}
                                             </h3>
                                             <p className="text-base line-clamp-2 text_gray_495 leading-[24px]">
-                                                {item.desc}
+                                                {item.detail}
                                             </p>
                                         </div>
 
                                         <div className="flex pt-5 items-center gap-2 text_gray_495 text-[12px] font-[Inter]">
-                                            <span>{item.timerequire}</span>
+                                            <span>{item.readTime}</span>
                                             <span className="w-[6px] h-[6px] bg-white rounded-full"></span>
                                             <time dateTime={item.date}>{item.date}</time>
                                         </div>
